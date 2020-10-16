a4dc97dbc20f605d48cccb25c8c8b7ffdd8f1da6
Maxim Davydov <maxim.davydov.94@gmail.com>
2020-10-16 12:01:18 +0700

    Add routes

---
+++ src/router.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const routes = [
+  {
+    path: '/',
+    name: 'auth',
+    meta: {layout: 'empty'},
+    component: () => import('./pages/Auth.vue')
+  },
+  {
+    path: '/analytics',
+    name: 'analytics',
+    meta: {layout: 'empty'},
+    component: () => import('./pages/Analytics.vue')
+  },
+]
+
+const router = new  Router({
+  mode: 'history',
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/analytics') {
+    const siteId = localStorage.getItem('leadhit-site-id');
+
+    if (!siteId) {
+      next('/')
+    }
+  }
+
+  next()
+})
+
+export default router
